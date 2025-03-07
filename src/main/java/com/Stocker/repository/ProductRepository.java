10d9a3c7e492642bf97ae4829384f11253fc62a7
Kaynã Mesquita <mesquitakayna@gmail.com>
2025-03-07 12:11:33 -0300

    Adiciona repositório produto

---
+++ src/main/java/com/Stocker/repository/ProductRepository.java
@@ -0,0 +1,58 @@
+package com.Stocker.repository;
+
+import com.Stocker.entity.Product;
+import com.Stocker.entity.User;
+import com.Stocker.util.HibernateUtil;
+
+import jakarta.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.query.SelectionQuery;
+
+import java.util.List;
+
+public class ProductRepository extends BaseRepository<Product, Long> {
+
+    public ProductRepository() {
+        super(Product.class);
+    }
+    
+    public Product getByBarcode(Long barcode) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query query = session.createQuery("FROM Product WHERE barcode=:barcode", Product.class);
+            query.setParameter("barcode", barcode);
+
+            Product product = (Product) query.getSingleResult();
+
+            return product;
+        } 
+        catch (jakarta.persistence.NoResultException e) {
+            return null;
+        }
+    }
+
+    public List<Product> getAll(User user) {
+        return getAll(user, null);
+    }
+
+    public List<Product> getAll(User user, String name) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("FROM Product p JOIN p.user u WHERE u.id=:u_id ");
+
+            if (name != null) sb.append("AND p.name LIKE :name");
+
+            SelectionQuery<Product> query = session.createSelectionQuery(
+                sb.toString(), 
+                Product.class
+            );
+
+            query.setParameter("u_id", user.getId());
+            if (name != null) query.setParameter("name", '%' + name + '%');
+
+            List<Product> products = query.list();
+
+            return products;
+        }
+    }
+}
---
+++ src/test/java/com/Stocker/repository/ProductRepositoryTest.java
@@ -0,0 +1,109 @@
+package com.Stocker.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import com.Stocker.entity.Product;
+import com.Stocker.entity.User;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(OrderAnnotation.class)
+public class ProductRepositoryTest {
+    
+    ProductRepository productRepository;
+    UserRepository userRepository;
+    Long mockBarcode;
+    String mockName;
+    Product mockProduct;
+    User mockUser;
+
+    @BeforeAll
+    void setup() {
+        productRepository = new ProductRepository();
+        userRepository = new UserRepository();
+        mockBarcode = 154184L;
+        mockName = "Sabonete";
+        mockUser = new User(null, "Pablo", "111.111.111-11", "a@gmail.com", "123456", "(84)99999-9999", null);
+        mockProduct = new Product(null, mockBarcode, mockName, 1, 5, 20, new Date(), mockUser, null);
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Cria Produto")
+    void createProduct() {        
+        userRepository.save(mockUser);
+        Long id = productRepository.save(mockProduct).getId();
+        
+        assertNotNull(id);
+        mockProduct.setId(id);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Recupera Produto com código de barra")
+    void getProduct() {
+        Product returnProduct = productRepository.getByBarcode(mockBarcode);
+
+        assertEquals(returnProduct.getAmount(),        mockProduct.getAmount());
+        assertEquals(returnProduct.getName(),          mockProduct.getName());
+        assertEquals(returnProduct.getPurchasePrice(), mockProduct.getPurchasePrice());
+        assertEquals(returnProduct.getSellingPrice(),  mockProduct.getSellingPrice());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Lista Produtos")
+    void listProduct() {
+        List<Product> returnProducts = productRepository.getAll(mockUser);
+
+        assertNotEquals(returnProducts.size(), 0);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Lista Produtos por nome")
+    void listProductByName() {
+        List<Product> returnProducts = productRepository.getAll(
+            mockUser,
+            mockName.substring(0, mockName.length() - 1)
+        );
+
+        assertNotEquals(returnProducts.size(), 0);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Atualiza Produto")
+    void updateProduct() {
+        mockProduct.setId(productRepository.getById(mockProduct.getId()).getId());
+        mockProduct.setName("test");
+        productRepository.update(mockProduct);
+
+        Product returnProduct = productRepository.getById(mockProduct.getId());
+        assertEquals(returnProduct.getName(), mockProduct.getName());
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Deleta Produto")
+    void deleteProduct() {
+        productRepository.delete(productRepository.getById(mockProduct.getId()).getId());
+
+        Product returnProduct = productRepository.getById(mockProduct.getId());
+        assertEquals(returnProduct, null);
+
+        userRepository.delete(mockUser.getId());
+    }
+}
